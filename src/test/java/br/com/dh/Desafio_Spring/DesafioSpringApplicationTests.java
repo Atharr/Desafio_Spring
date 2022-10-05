755e002143d89b8387ccbccc72693886d81972f3
Marcello Pires Alves <marcello.alves@gmail.com>
2022-10-05 10:15:54 -0300

    commit inicial

---
+++ src/test/java/br/com/dh/Desafio_Spring/DesafioSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dh.Desafio_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
