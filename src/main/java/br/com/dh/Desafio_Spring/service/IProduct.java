b465db148442e2b157d67f28a87cd534a4c18355
Marcello Pires Alves <marcello.alves@gmail.com>
2022-10-05 13:28:50 -0300

    feat: product service

--- src/main/java/br/com/dh/Desafio_Spring/service/IProduct.java
+++ src/main/java/br/com/dh/Desafio_Spring/service/IProduct.java
@@ -1,2 +1,16 @@
-package br.com.dh.Desafio_Spring.service;public interface IProduct {
+package br.com.dh.Desafio_Spring.service;
+
+import br.com.dh.Desafio_Spring.exception.NotFoundException;
+import br.com.dh.Desafio_Spring.model.Product;
+
+import java.util.List;
+
+/**
+ * @interface IProduct
+ * @description Interface for the Product service
+ */
+public interface IProduct {
+  Product getProduct(String name) throws NotFoundException;
+  List<Product> getAll();
+  void save(Product newProduct);
 }
---
+++ src/main/java/br/com/dh/Desafio_Spring/service/ProductService.java
@@ -0,0 +1,37 @@
+package br.com.dh.Desafio_Spring.service;
+
+import br.com.dh.Desafio_Spring.exception.NotFoundException;
+import br.com.dh.Desafio_Spring.model.Product;
+import br.com.dh.Desafio_Spring.repository.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class ProductService implements IProduct {
+  @Autowired
+  private ProductRepo repo;
+
+  @Override
+  public List<Product> getAll() {
+    return repo.getAll().stream().collect(Collectors.toList());
+  }
+
+  @Override
+  public Product getProduct(String name) throws NotFoundException {
+    Optional<Product> product = repo.getProduct(name);
+
+    if (product.isEmpty()) {
+      throw new NotFoundException("Produto não encontrado.");
+    }
+    return product.get();
+  }
+
+  @Override
+  public void save(Product product) {
+    repo.save(product);
+  }
+}
