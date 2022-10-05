88a58dea92936ab2c60276856bba9300e4e2c5ea
Marcello Pires Alves <marcello.alves@gmail.com>
2022-10-05 14:08:44 -0300

    feat: Product Controller (1o. GET)

---
+++ src/main/java/br/com/dh/Desafio_Spring/controller/ProductController.java
@@ -0,0 +1,24 @@
+package br.com.dh.Desafio_Spring.controller;
+
+import br.com.dh.Desafio_Spring.dto.ProductDTO;
+import br.com.dh.Desafio_Spring.service.IProduct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/articles")
+public class ProductController {
+  @Autowired
+  private IProduct service;
+
+  @GetMapping
+  public ResponseEntity<List<ProductDTO>> getAll() {
+    return new ResponseEntity<>(service.getAll(), HttpStatus.OK);
+  }
+}
