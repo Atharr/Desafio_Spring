da0b037b140d8432d6e75c2d8e4804720e24ebfa
Marcello Pires Alves <marcello.alves@gmail.com>
2022-10-05 13:29:33 -0300

    feat: exception handling

--- src/main/java/br/com/dh/Desafio_Spring/exception/ExceptionDetails.java
+++ src/main/java/br/com/dh/Desafio_Spring/exception/ExceptionDetails.java
@@ -1,2 +1,15 @@
-package br.com.dh.Desafio_Spring.exception;public class ExceptionDetails {
+package br.com.dh.Desafio_Spring.exception;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Builder
+@Data
+public class ExceptionDetails {
+  private String title;
+  private int status;
+  private String message;
+  private LocalDateTime timeStamp;
 }
--- src/main/java/br/com/dh/Desafio_Spring/exception/NotFoundException.java
+++ src/main/java/br/com/dh/Desafio_Spring/exception/NotFoundException.java
@@ -1,2 +1,11 @@
-package br.com.dh.Desafio_Spring.exception;public class NotFoundException {
+package br.com.dh.Desafio_Spring.exception;
+
+/**
+ * @class NotFoundException
+ * @classdesc Custom exception for not found events.
+ */
+public class NotFoundException extends RuntimeException {
+  public NotFoundException(String message) {
+    super(message);
+  }
 }
